0e042476205291208dd3c6e4c69a1a5ffcea5154
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-01-28 18:39:12 -0500

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,28 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+
+var cards = ["queen", "queen", "king", "king"];
+
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+
+var cardsInPlay = [];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (length.cardsInPlay === 2) {
+
+}
+else if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
