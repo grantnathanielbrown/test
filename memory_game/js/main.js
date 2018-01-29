9af11d29867ae94bee7c82665570e5b37ba00dd6
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-01-29 01:57:08 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,31 @@ console.log("Up and running!");
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+
+
+
+]
 
 
 
@@ -19,15 +43,36 @@ console.log("Sorry, try again.");
 }
 function flipCard(cardId) {
 	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
-
+	console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards.cardImage);
+console.log(cards.suit);
 }
 
 flipCard(0);
 flipCard(2);
 
 
+
+
+
+
+
+
+
+/*
+var myArray = [
+{
+color: 'red',
+flower: 'rose',
+petals: 20
+},
+{
+color: 'blue',
+flower: 'violet',
+petals: 6
+}
+];*/
 
 /* 
 
